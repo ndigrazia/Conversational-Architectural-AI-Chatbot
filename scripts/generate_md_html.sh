aec188199820f5f62d70418a4e49999050a41ccd
m_margot <mauricio.margot@telefonica.com>
2024-03-27 17:32:59 -0300

    pandoc --embed-resources elimination and relative paths ussage

--- scripts/generate_md_html.sh
+++ scripts/generate_md_html.sh
@@ -1,5 +1,5 @@
 
 #! /bin/sh
-
+chmod +x scripts/script_interno_generar_html.sh
 chmod +x scripts/script_interno_pandoc.sh
 docker run --rm --volume "./adrs:/data" --volume "./scripts:/scripts" --user `id -u`:`id -g` --entrypoint "/scripts/script_interno_pandoc.sh" pandoc/latex:latest-ubuntu
--- scripts/script_interno_pandoc.sh
+++ scripts/script_interno_pandoc.sh
@@ -1,11 +1,25 @@
 #! /bin/sh
 ls /scripts/pandoc.css
 
+echo "Cambiando referencias a .md.html"
+
 # - Con este sed, se cambian todos lo links a ".md.html" ej: "(./nombre-archivo.md.html)"
 find . -name "*.md" -exec sed -i 's/.md/.md.html/g' {} \;
 
+echo "agregando . a links relativos"
 # - Con este sed, se agrega el "./" inicial a la referencia de archivo ej: "(./nombre-archivo.md.pdf)"
 # cuando la misma no existe
 find . -name "*.md" -exec sed -i 's/(\([A-Za-z0-9\-]\+\).md.html)/(.\/\1.md.html)/g' {} \;
 
-find . -name "*.md" -execdir pandoc -t html -c /scripts/pandoc.css -s --embed-resources {} -o {}.html \;
+# copio .css
+cp /scripts/pandoc.css ./
+
+echo "generando html"
+# genedo los html
+ROOT_PATH=$PWD
+
+find . -name "*.md" -execdir /scripts/script_interno_generar_html.sh {} {}.html $ROOT_PATH \;
+
+
+
+
